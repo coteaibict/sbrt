25e1a0b066220e56f0c9519abf94e39c0de57c1b
Daniella Angelos <daniaangelos@gmail.com>
2018-07-24 14:46:08 -0300

    criando rotas

--- sbrt-server/src/routes/crmRoutes.ts
+++ sbrt-server/src/routes/crmRoutes.ts
@@ -1,17 +1,41 @@
 import {Request, Response} from "express";
 
 export class Routes {       
-    public routes(app): void {          
-        app.route('/')
-        .get((req: Request, res: Response) => {            
-            res.status(200).send({
-                message: 'GET request successfulll!!!!'
-            })
-        })               
+  public routes(app): void {          
+    app.route('/')
+    .get((req: Request, res: Response) => {            
+      res.status(200).send({
+        message: 'GET request successfulll!!!!'
+      })
+    })               
 
-        app.route('/pergunta')
-        .get()
-        .post()
+    app.route('/pergunta')
+    .get((req: Request, res: Response) => {
+      res.status(200).send({
+        message: 'GET request successfull.'
+      })
+    })
+    .post((req: Request, res: Response) => {
+      res.status(200).send({
+        message: 'POST request successfull.'
+      })
+    })
+    app.route('/pergunta/:perguntaId')
+    .get((req: Request, res: Response) => {
+      res.status(200).send({
+        message: 'GET request successfull'
+      })
+    })
+    .put((req: Request, res: Response) => {
+      res.status(200).send({
+        message: 'PUT request successfull.'
+      })
+    })
+    .delete((req: Request, res: Response) => {
+      res.status(200).send({
+        message: 'DEL request successfull.'
+      })
+    })
 
-    }
+  }
 }
